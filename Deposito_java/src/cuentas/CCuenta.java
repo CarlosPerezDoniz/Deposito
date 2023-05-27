5fc3994ab5d850409dde50cd77db28d54db667d8
Carlos Perez Doniz <carlosdavid1977@gmail.com>
2023-05-27 11:13:41 +0100

    Actualizo el proyecto con los comentarios para generar el documento de java doc

--- Deposito_java/src/cuentas/CCuenta.java
+++ Deposito_java/src/cuentas/CCuenta.java
@@ -1,5 +1,16 @@
 package cuentas;
 
+/**
+ * @author Carlos D. Pérez Dóniz
+ * @version 1.5
+ * 
+ */
+
+/** 
+ * Clase para realizar las distintas operaciones que se pueden realizar en la cuenta corriente
+ * tanto ingresos como retiradas de efectivo
+ */
+
 public class CCuenta {
 
 	private String nombre;
@@ -7,56 +18,125 @@ public class CCuenta {
 	private double saldo;
 	private double tipoInterés;
 
+	/**
+	 * Constructor vacio de la clase CCuenta.
+	 */
 	public CCuenta() {
 	}
+	
+	/**
+	 * Constructor con parámetros de la clase CCuenta.
+	 * @param nom Nombre del titular de la cuenta.
+	 * @param cue Número de cuenta.
+	 * @param sal El saldo actual de la cuenta.
+	 * @param tipo El tipo de interés.
+	 */
 
 	public CCuenta(String nom, String cue, double sal, double tipo) {
 		nombre = nom;
 		cuenta = cue;
 		saldo = sal;
 	}
+	
+	/**
+	 * Para obtener el nombre del titular.
+	 * @return nombre.
+	 */
 
 	public String getNombre() {
 		return nombre;
 	}
+	
+	/**
+	 * Para modificar el nombre del titular.
+	 * @param nombre.
+	 */
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
+	/**
+	 * Para obtener el número de cuenta.
+	 * @return cuenta.
+	 */
 
 	public String getCuenta() {
 		return cuenta;
 	}
 
+	/**
+	 * Para modificar el número de cuenta.
+	 * @param cuenta
+	 */
+	
 	public void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
+	
+	/**
+	 * Para obterner el saldo de la cuenta.
+	 * @return saldo.
+	 */
 
 	public double getSaldo() {
 		return saldo;
 	}
+	
+	/**
+	 * Para modificar el saldo de la cuenta.
+	 * @param saldo
+	 */
 
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
+	
+	/**
+	 * Para consultar el tipo de interés.
+	 * @return tipo de interés.
+	 */
 
 	public double getTipoInterés() {
 		return tipoInterés;
 	}
+	
+	/**
+	 * Para establecer el tipo de interés.
+	 * @param tipoInterés
+	 */
 
 	public void setTipoInterés(double tipoInterés) {
 		this.tipoInterés = tipoInterés;
 	}
+	
+	/**
+	 * Para obtener el estado del saldo de la cuenta.
+	 * @return saldo de la cuenta.
+	 */
 
 	public double estado() {
 		return saldo;
 	}
+	
+	/**
+	 * Método para realizar un ingreso de efectivo en la cuenta corriente.
+	 * @param cantidad La cantidad de efectivo a ingresar.
+	 * @throws Exception por si la cantidad a ingresar es negativa.
+	 */
 
 	public void ingresar(double cantidad) throws Exception {
 		if (cantidad < 0)
 			throw new Exception("No se puede ingresar una cantidad negativa");
 		saldo = saldo + cantidad;
 	}
+	
+	/**
+	 * Método para realizar una retirada de efectivo de la cuenta corriente.
+	 * @param cantidad La cantidad de efectivo retirada.
+	 * @throws Exception por si la cantidad a retirar es negativa o no hay suficiente saldo en la cuenta
+	 * para la cantidad a retirar.
+	 */
 
 	public void retirar(double cantidad) throws Exception {
 		if (cantidad <= 0)
--- Deposito_java/src/cuentas/Main.java
+++ Deposito_java/src/cuentas/Main.java
@@ -1,20 +1,33 @@
 package cuentas;
 
+/** 
+ * 
+ * @author Carlos D. Pérez Dóniz
+ * @version 1.5
+ * 
+ */
+
 public class Main {
 
 	public static void main(String[] args) {
 		CCuenta cuenta1;
         double saldoActual;
 
+        /** Datos iniciales de la cuenta con el nombre, nº de cuenta y saldo inicial*/
+        
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es " + saldoActual);
         
     
-        	operativa_cuenta(cuenta1,100);
-        
+        	operativa_cuenta(cuenta1,100);  
 	}
 
+	/** Método que realiza ingresos y retiradas de efectivo de la cuenta corriente dando al final el 
+	 * saldo actualizado
+	 * @param cuenta1
+	 * @param cantidad
+	 */
 	public static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
 		
 		
